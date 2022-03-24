426ebe5cb5a190d2149f956246e882ad9316873d
Charlie Heller <charlieheller95@gmail.com>
2022-03-24 16:44:12 +0100

    add pycall dep., add GMM_labels function, remove ipynb checkpoints and edit .gitignore

--- src/.ipynb_checkpoints/_HMMs-checkpoint.jl
+++
@@ -1,7 +0,0 @@
-module _HMMs
-
-export HMM
-
-include("ARHMM.jl")
-
-end # module
--- src/ARHMM.jl
+++ src/ARHMM.jl
@@ -1,4 +1,4 @@
-
+using PyCall
 ##K # number of hidden states
 #T # total number of timestamps
 #A # Transition probability matrix
@@ -218,3 +218,11 @@ function viterbi(hmm::HMM)
     end
 end
 
+function GMM_labels(no_components, cur_Y)
+    mixture = pyimport("sklearn.mixture")
+    gmm = mixture[:GaussianMixture](no_components, covariance_type="full")
+    gmm[:fit](cur_Y)
+    mixture_component_labels = zeros(Int64, size(cur_Y)[1])
+    mixture_component_labels .= gmm[:predict](cur_Y);  # labels to feed in the HMM
+    mixture_component_labels .+= 1
+end
